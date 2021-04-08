97e6933a84cea0a70a0d3143b3c8304363b13dc8
camilleroquencourt <camille.roquencourt@hotmail.fr>
2021-04-08 14:40:00 +0200

    extended description, added inst/script/script.R for describe inst/ext data, added installation and sessionInfo to vignette

---
+++ inst/script/script.R
@@ -0,0 +1,11 @@
+#' mycobacteria dataset
+#' This dat set was generated with a proton-transfer-reaction
+#' quadrupole time-of-flight mass spectrometer (Ionicon Analytik GmbH,
+#' Innsbruck, Austria). The air in the head space of the cells culture has been
+#' analyze after 1 week.
+#'
+#' exhaled air
+#' Two volunteers blow into the mass spectrometer (Ionicon Analytik GmbH,
+#' Innsbruck, Austria) with a BET-med.
+
+
--- tests/testthat/test_data.R
+++ tests/testthat/test_data.R
@@ -2,16 +2,21 @@ testthat::context("Test data.")
 
 test_data <- function() {
   directory <-  system.file("extdata",  package = "ptairData")
-  testthat::expect_equal( list.files(directory) , c("exhaledAir"  , "mycobacteria"))
-  testthat::expect_equal( list.files(file.path(directory,"exhaledAir")) , c("ind1"  , "ind2"))
-  testthat::expect_equal( list.files(file.path(directory,"exhaledAir/ind1")) , c("ind1-1.h5"  , "ind1-2.h5","ind1-3.h5"))
-  testthat::expect_equal( list.files(file.path(directory,"mycobacteria")), c("Control" , "Specie-a", "Specie-b"))
-  testthat::expect_equal( list.files(file.path(directory,"mycobacteria"),recursive = TRUE), c("Control/Control1.h5" ,
-                                                                                              "Control/Control2.h5" ,
-                                                                                              "Specie-a/Specie-a1.h5" ,
-                                                                                              "Specie-a/Specie-a2.h5" ,
-                                                                                              "Specie-b/specie-b1.h5",
-                                                                                              "Specie-b/specie-b2.h5"))
+  testthat::expect_equal( list.files(directory) , c("exhaledAir"  ,
+                                                    "mycobacteria"))
+  testthat::expect_equal( list.files(file.path(directory,"exhaledAir")) ,
+                          c("ind1"  , "ind2"))
+  testthat::expect_equal( list.files(file.path(directory,"exhaledAir/ind1")) ,
+                          c("ind1-1.h5"  , "ind1-2.h5","ind1-3.h5"))
+  testthat::expect_equal( list.files(file.path(directory,"mycobacteria")),
+                          c("Control" , "Specie-a", "Specie-b"))
+  testthat::expect_equal( list.files(file.path(directory,"mycobacteria"),
+                                     recursive = TRUE), c("Control/Control1.h5" ,
+                                                          "Control/Control2.h5" ,
+                                                          "Specie-a/Specie-a1.h5" ,
+                                                          "Specie-a/Specie-a2.h5" ,
+                                                          "Specie-b/specie-b1.h5",
+                                                          "Specie-b/specie-b2.h5"))
 
 }
 
